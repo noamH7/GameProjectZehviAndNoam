c00612398fc3a03f301055663742bff37b1b1506
נעם ברוכי <nbaruchi@g.jct.ac.il>
2024-02-11 04:44:52 +0200

    continue trivia

---
+++ JS/trivia.js
@@ -0,0 +1,6 @@
+const startGame = document.getElementById('start-game');
+const questions = document.getElementsByClassName('question');
+//function nextQuestion(){
+//questions[0].style.visibility = "visible";
+//}
+console.log(questions);
